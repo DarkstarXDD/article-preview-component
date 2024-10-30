1077110468403f15dfcaf0c8c2d34d395379192f
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-10-31 00:18:21 +0530

    refactor: update js

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
+// Get elements
 const bottomWrapper = document.getElementById("bottom-wrapper")
 const authorDetails = document.getElementById("author-details")
 const shareDetails = document.getElementById("share-details")
 const shareButton = document.getElementById("share-button")
-let isExpanded = false
+
+function openPopover() {
+  shareButton?.setAttribute("aria-expanded", "true")
+}
+
+function closePopover() {
+  shareButton?.setAttribute("aria-expanded", "false")
+}
 
 shareButton?.addEventListener("click", () => {
-  let prevIsExpanded = shareButton.getAttribute("aria-expanded")
+  let isPopoverOpen = shareButton.getAttribute("aria-expanded")
 
-  if (prevIsExpanded === "true") {
-    shareButton.setAttribute("aria-expanded", "false")
-    isExpanded = !isExpanded
+  // If the Popover is currently open, close it. If not open it.
+  if (isPopoverOpen === "true") {
+    closePopover()
   } else {
-    shareButton.setAttribute("aria-expanded", "true")
-    isExpanded = !isExpanded
+    openPopover()
   }
   bottomWrapper?.classList.toggle("bottom-expanded")
   authorDetails?.classList.toggle("details-hidden")
